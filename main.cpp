54772a2e1756d5025cc1d398c648ed21462d9cb6
hidayahsidiq <muhamads03@gmail.com>
2025-04-28 20:23:23 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "StopwatchWindow.h"
+#include <gtkmm/application.h>
 
 int main(int argc, char* argv[]) {
-    auto app = Gtk::Application::create("org.gtkmm.rubik.timer");
+    auto app = Gtk::Application::create("org.example.rubikstimer");
+
+    StopwatchWindow window;
     return app->make_window_and_run<StopwatchWindow>(argc, argv);
 }
